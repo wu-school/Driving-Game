1844684a3837a73e4e73f87bddba8b4329dea0d5
Phoenix Wu <phoenixwuschoolstuff@gmail.com>
2022-09-20 14:35:30 -0500

    class ended

--- Driving Game/Assets/Scripts/DrivingControls.cs
+++ Driving Game/Assets/Scripts/DrivingControls.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class DrivingControls : MonoBehaviour
 {
+    [SerializeField] float acceration, torque, maxSpeed;
+
+    public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+            rb = GetComponent<Rigidbody2D>();
+            acceration = 5;
+            maxSpeed = 10;
+            torque = 5;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if(Input.GetKey(KeyCode.W) && rb.velocity.magnitude<maxSpeed){
+            rb.velocity+=( rb.velocity.normalized*acceration/100);
+        }  
+        else if(Input.GetKey(KeyCode.S) && rb.velocity.magnitude<maxSpeed){
+            rb.velocity*=( rb.velocity.normalized*-1*acceration/100);
+        }
+        
+        if(Input.GetKey(KeyCode.D)){
+            float turn = Input.GetAxis("Horizontal");
+            rb.AddTorque(torque*turn*-1);
+        } else if(Input.GetKey(KeyCode.A)){
+            float turn = Input.GetAxis("Horizontal");
+            rb.AddTorque(torque*turn);
+        } else{
+            
+        }
         
     }
 }
