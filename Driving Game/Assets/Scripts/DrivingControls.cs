49a37b71a2ec7058882498cc601b1665610bd167
Phoenix Wu <phoenixwuschoolstuff@gmail.com>
2022-09-21 14:35:29 -0500

    class ended

--- Driving Game/Assets/Scripts/DrivingControls.cs
+++ Driving Game/Assets/Scripts/DrivingControls.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DrivingControls : MonoBehaviour
 {
-    [SerializeField] float acceration, torque, maxSpeed;
+    [SerializeField] float acceration, torque, maxSpeed, speed, maxAngularVelocity;
 
     public Rigidbody2D rb;
 
@@ -12,30 +12,35 @@ public class DrivingControls : MonoBehaviour
     void Start()
     {
             rb = GetComponent<Rigidbody2D>();
-            acceration = 5;
-            maxSpeed = 10;
-            torque = 5;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(Input.GetKey(KeyCode.W) && rb.velocity.magnitude<maxSpeed){
-            rb.velocity+=( rb.velocity.normalized*acceration/100);
-        }  
-        else if(Input.GetKey(KeyCode.S) && rb.velocity.magnitude<maxSpeed){
-            rb.velocity*=( rb.velocity.normalized*-1*acceration/100);
-        }
+        speed = rb.velocity.magnitude;
         
-        if(Input.GetKey(KeyCode.D)){
+    
+        if(Input.GetKey(KeyCode.D) && rb.angularVelocity<maxAngularVelocity){
             float turn = Input.GetAxis("Horizontal");
             rb.AddTorque(torque*turn*-1);
-        } else if(Input.GetKey(KeyCode.A)){
+        } else if(Input.GetKey(KeyCode.A) && -1*rb.angularVelocity<maxAngularVelocity){
             float turn = Input.GetAxis("Horizontal");
-            rb.AddTorque(torque*turn);
+            rb.AddTorque(torque*turn*-1);
         } else{
-            
+          rb.angularVelocity = 0;  
         }
-        
+
+        if(Input.GetKey(KeyCode.W) && speed<maxSpeed){
+            Vector2 deltaV = transform.up*acceration/100;
+            rb.velocity+=deltaV;
+        }  
+        else if(Input.GetKey(KeyCode.S) && speed>0 && speed<maxSpeed){
+            Vector2 deltaV = transform.up*acceration/100;
+            rb.velocity-=deltaV;
+        } else if (Vector2.Dot(rb.velocity, transform.up)>0){
+            rb.velocity = transform.up*speed;
+        } else {
+            rb.velocity = -transform.up*speed;
+        }
     }
 }
